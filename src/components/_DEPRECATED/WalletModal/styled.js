f32c7758598ee9a3af5478c15928a638862dd019
LebdevIgor <igorlebedev018@gmail.com>
2023-12-18 09:18:38 +0000

    TM-202

--- src/components/_DEPRECATED/WalletModal/styled.js
+++ src/components/_DEPRECATED/WalletModal/styled.js
@@ -47,7 +47,9 @@ export const ModalBlur = styled.div`
 		position: absolute;
 		width: 100%;
 		height: 100%;
-		z-index: 10;
+		z-index: 100;
+		background-color: ${COLORS.TRANSPARENT_BLACK};
+		opacity: 0.75;
 	}
 `;
 
